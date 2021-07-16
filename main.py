f9211d7b1ebff025c6bdf323fbed797faeb81cd9
Alexey Petrov <a.petrov@energochain.ru>
2021-07-16 13:14:58 +0300

    Add set_volume medhood

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import vlc
 from time import sleep
 
+
 class InternetRadio(object):
-    def __init__(self, volume=70):
-        self.volume = int(volume)
+    def __init__(self):
         self.stations = dict([
             ("test", "1.mp3"),
             ("Monte Carlo", "http://icecast.unitedradio.it/RMC.mp3"),
@@ -11,10 +11,8 @@ class InternetRadio(object):
             ("Книга", "http://bookradio.hostingradio.ru:8069/fm")])
         self.player = vlc.MediaPlayer()
 
-
     def play(self, station):
         self.player.set_mrl(self.stations[station])
-        self.player.audio_set_volume(self.volume)
         self.player.play()
         sleep(5)  # Or however long you expect it to take to open vlc
         while self.player.is_playing():
@@ -22,11 +20,12 @@ class InternetRadio(object):
 
     def set_volume(self, volume):
         if isinstance(volume, int) and 0 <= volume <= 100:
-            self.volume = int(volume)
+            self.player.audio_set_volume(volume)
             return True
         else:
             return False
 
 
-r = InternetRadio()
-r.play("Monte Carlo")
+p = InternetRadio()
+p.set_volume(70)
+p.play("Monte Carlo")
