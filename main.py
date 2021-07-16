1c6dddd829cd383108d636e093047a0df69e4c46
Alexey Petrov <a.petrov@energochain.ru>
2021-07-16 16:39:52 +0300

    Stop was added

--- main.py
+++ main.py
@@ -1,31 +1,40 @@
-import vlc
+import vlc, time
 from time import sleep
 
 
 class InternetRadio(object):
-    def __init__(self):
+    def __init__(self, valume=60):
+        self.volume = int(valume)
         self.stations = dict([
             ("test", "1.mp3"),
             ("Monte Carlo", "http://icecast.unitedradio.it/RMC.mp3"),
             ("Jazz", "http://nashe1.hostingradio.ru:80/jazz-256"),
             ("Книга", "http://bookradio.hostingradio.ru:8069/fm")])
         self.player = vlc.MediaPlayer()
+        self.set_volume(self.volume)
+
 
     def play(self, station):
         self.player.set_mrl(self.stations[station])
         self.player.play()
-        sleep(5)  # Or however long you expect it to take to open vlc
-        while self.player.is_playing():
-            sleep(1)
+
 
     def set_volume(self, volume):
         if isinstance(volume, int) and 0 <= volume <= 100:
             self.player.audio_set_volume(volume)
+            self.volume = volume
             return True
         else:
             return False
 
 
+    def stop(self):
+        self.set_volume(0)
+
+
 p = InternetRadio()
-p.set_volume(70)
 p.play("Monte Carlo")
+
+while True:
+    newVolume = int(input())
+    p.set_volume(newVolume)
